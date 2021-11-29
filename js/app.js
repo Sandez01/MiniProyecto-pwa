3230acf063b8b024af329a8b43477d535b411def
Sandez01 <48538346+Sandez01@users.noreply.github.com>
2021-11-29 13:52:19 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator){
     window.addEventListener('Load', () =>{
-        navigator.serviceWorker.register('.../sw.js').then(() => {
+        navigator.serviceWorker.register('../sw.js').then(() => {
             console.log('Soy el service Worker registrado y funcionando.')
         })
     })
